0fcc1b46fc0206c0d65b85333463f1bbd69c5902
Amy Rymer <amy.rymer@data-vault.com>
2022-04-21 22:25:18 +0000

    analysis

--- data_generator/analysis.py
+++ data_generator/analysis.py
@@ -2,19 +2,20 @@ from data_generator.utils import flight_data_to_df
 from collections import Counter
 
 flight_data = flight_data_to_df()
+flight_data = flight_data.drop(flight_data.columns[[3, 4, 5, 6, 7, 9, 10, 12]], axis=1)
+flight_data.columns = range(flight_data.columns.size)
+
 
 airport_name = flight_data.iloc[:, 0].tolist()
 city_name = flight_data.iloc[:, 1].tolist()
 country_name = flight_data.iloc[:, 2].tolist()
-iata = flight_data.iloc[:, 3].tolist()
-icao = flight_data.iloc[:, 4].tolist()
-latitude = flight_data.iloc[:, 5].tolist()
-longitude = flight_data.iloc[:, 6].tolist()
-altitude = flight_data.iloc[:, 7].tolist()
-timezone = flight_data.iloc[:, 8].tolist()
-dst = flight_data.iloc[:, 9].tolist()
-tz_database_timezone = flight_data.iloc[:, 10].tolist()
-type_of_port = flight_data.iloc[:, 11].tolist()
-source = flight_data.iloc[:, 12].tolist()
+timezone = flight_data.iloc[:, 3].tolist()
+type_of_port = flight_data.iloc[:, 4].tolist()
 
-print(len(set(Counter(airport_name))))
+# flight_data = flight_data[flight_data.iloc[:, 0].str.contains("unknown|\\N")==False]
+# flight_data = flight_data[flight_data.iloc[:, 1].str.contains("unknown|\\N")==False]
+# flight_data = flight_data[flight_data.iloc[:, 2] != 'unknown']
+# flight_data = flight_data[flight_data.iloc[:, 3] != 'unknown']
+# flight_data = flight_data[flight_data.iloc[:, 4] != 'unknown']
+
+print(Counter(type_of_port))
